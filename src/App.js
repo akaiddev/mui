c0003ebbd8270c9f740a09f6075504302c22e816
Abdur Rahman <akaid.dev@gmail.com>
2022-02-09 14:06:10 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// routes
+import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
+// components
+import ScrollToTop from './components/ScrollToTop';
+import Router from './routes';
+// theme
+import ThemeConfig from './theme';
+import GlobalStyles from './theme/globalStyles';
+
+// ----------------------------------------------------------------------
+
+export default function App() {
+  return (
+    <ThemeConfig>
+      <ScrollToTop />
+      <GlobalStyles />
+      <BaseOptionChartStyle />
+      <Router />
+    </ThemeConfig>
+  );
+}
